60f4c5c4566714620de43b268ac02b27019b5edc
Jonas Silva <jonasgenro@gmail.com>
2023-05-02 11:46:01 -0300

    :recycle: alterando nomes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
 import Experience from "./Components/Experience/Experience";
 import Footer from "./Components/Footer/Footer";
 import NavBar from "./Components/Navbar/NavBar";
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <Home />
+      <About />
       <Experience />
       <Projects />
       <Footer />
